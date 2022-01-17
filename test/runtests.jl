193e8033ef0f6680740725d2a5c3f830d95d8f72
Frank Urbach <33724682+FrankUrbach@users.noreply.github.com>
2022-01-17 20:59:18 +0100

    Setting up test environment  (#6)
    * changed according requests in PR#6
    
    * missing package Pkg added
    
    * Changed the tests and the project according
    Adrians requirements
    
    * Pkg added to test Project.toml
    
    * added Dates

---
+++ test/runtests.jl
@@ -0,0 +1,18 @@
+cd(@__DIR__)
+
+using Pkg
+
+using Test, TestSetExtensions, SafeTestsets
+using SearchLight
+using SearchLightMySQL
+using Dates
+
+# @testset ExtendedTestSet "SearchLight PostgreSQL adapter tests" begin
+#   @includetests ARGS
+# end
+
+# run a simple connect test for the first time
+
+@testset ExtendedTestSet "SearchLight tests" begin
+    @includetests ARGS
+end
